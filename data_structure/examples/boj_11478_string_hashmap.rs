73c0d67dbcc673cf2ba4b0038ffd0ae25426e5d1
jaemin <datactress@gmail.com>
2022-12-16 23:52:32 +0900

    feat: algorithm

---
+++ data_structure/examples/boj_11478_string_hashmap.rs
@@ -0,0 +1,28 @@
+// O(n^2)
+
+use std::io::{self, prelude::*, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let mut output = BufWriter::new(io::stdout().lock());
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    let v = buffer.trim();
+    let n = v.len();
+    let mut hashset = HashSet::new();
+
+    let mut elements = 1;
+    let mut result = 0;
+    while elements <= n {
+        let mut i = 0;
+        hashset.clear();
+        while i + elements <= n {
+            hashset.insert(&v[i..i + elements]);
+            i += 1;
+        }
+        elements += 1;
+        result += hashset.len();
+    }
+    writeln!(output, "{}", result);
+}
---
+++ data_structure/target/debug/examples/boj_11478_string_hashmap.d
@@ -0,0 +1,1 @@
+/home/jam/workspace/rust/training/ps/data_structure/target/debug/examples/boj_11478_string_hashmap: /home/jam/workspace/rust/training/ps/data_structure/examples/boj_11478_string_hashmap.rs
