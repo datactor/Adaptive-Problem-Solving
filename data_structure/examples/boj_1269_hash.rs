1a82b6095d1d40ac96c4c9ce4ea2e1c378b68ade
jaemin <datactress@gmail.com>
2022-12-16 17:38:48 +0900

    feat: algorithm

---
+++ data_structure/examples/boj_1269_hash.rs
@@ -0,0 +1,36 @@
+// O(N)
+
+use std::io::{self, prelude::*, BufWriter};
+use std::collections::HashSet;
+
+fn main() {
+    let mut output = BufWriter::new(io::stdout().lock());
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut fst_set = buffer.split_ascii_whitespace().map(
+        |s| s.parse::<usize>().unwrap()).collect::<HashSet<_>>();
+
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut snd_set = buffer.split_ascii_whitespace().map(
+        |s| s.parse::<usize>().unwrap()).collect::<HashSet<_>>();
+
+    let mut sub = 0;
+    for i in &fst_set {
+        match snd_set.get(&i) {
+            None => sub += 1,
+            _ => {},
+        }
+    }
+
+    for i in &snd_set {
+        match fst_set.get(&i) {
+            None => sub += 1,
+            _ => {},
+        }
+    }
+    writeln!(output, "{}", sub);
+}
---
+++ data_structure/target/debug/examples/boj_1269_hash.d
@@ -0,0 +1,1 @@
+/home/jam/workspace/rust/training/ps/data_structure/target/debug/examples/boj_1269_hash: /home/jam/workspace/rust/training/ps/data_structure/examples/boj_1269_hash.rs
