5622fd0ddd881e2f322ed220194a2d0169c6531e
jaemin <datactress@gmail.com>
2022-12-27 10:06:56 +0900

    feat: backtracking with recursion

---
+++ backtracking/target/debug/examples/boj_15651-226cfcce455831c2.d
@@ -0,0 +1,5 @@
+/home/jam/workspace/rust/training/ps/backtracking/target/debug/examples/boj_15651-226cfcce455831c2: examples/boj_15651.rs
+
+/home/jam/workspace/rust/training/ps/backtracking/target/debug/examples/boj_15651-226cfcce455831c2.d: examples/boj_15651.rs
+
+examples/boj_15651.rs:
---
+++ backtracking/target/debug/examples/boj_15651.d
@@ -0,0 +1,1 @@
+/home/jam/workspace/rust/training/ps/backtracking/target/debug/examples/boj_15651: /home/jam/workspace/rust/training/ps/backtracking/examples/boj_15651.rs
