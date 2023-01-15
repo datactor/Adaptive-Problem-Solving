46ab34b3ce760cd52e2f0b8cf4569671ca54dbb6
jaemin <datactress@gmail.com>
2023-01-15 20:18:09 +0900

    feat: backtracking & recursion & dfs

--- backtracking/examples/boj_2580.rs
+++ backtracking/examples/boj_2580.rs
@@ -22,9 +22,6 @@ fn main() -> Result<(), Box<dyn Error>> {
         })
         .collect();
 
-    println!("{:?}", v);
-    println!("{:?}", blank);
-
     // blank에 있는 튜플을 idx 순서대로 탐색
     dfs(0, &blank, &mut v, &mut output);
 
@@ -67,19 +64,22 @@ fn dfs(idx: usize, blank: &Vec<(usize, usize)>, v: &mut Vec<Vec<usize>>, output:
             }
             write!(output, "\n").unwrap();
         }
-        output.flush().unwrap();
-        std::process::exit(0); // 스도쿠를 하나라도 완료되면 프로세스 종료.
+
+        output.flush().unwrap(); // process를 강제 종료하면 write가 완료되지 않고 끝나기 때문에 flushing 해줌.
+                                 //
+                                 // return으로 종료하지 않는 이유? 여러 dfs중 하나만 종료되고 다른 dfs는 진행되어
+                                 // 추가적인 스도쿠를 완료할 수 도 있음
+        std::process::exit(0); // 스도쿠를 하나라도 완료하면 프로세스 종료.
     }
     let (x, y) = (blank[idx].0, blank[idx].1);
 
     for i in 1..10 {
-        // row, col, sqr 모두를 계산해봐서 v[x][y] 자리에 들어갈 수 있는 i를 찾는다(for i in 1..10).
-        // i를 찾으면, v[x][y]의 값을 i로 바꾸고, 다음 dfs를 실행한다.
+        // 1. row, col, sqr 모두를 탐색해보고 v[x][y]자리에 들어갈 수 있는 i를 찾는다(for i in 1..10).
+        //    i를 찾으면, v[x][y]의 값을 i로 바꾸고, 다음 dfs를 실행한다.
         // -- 반복 --
         //
-        // dfs가 끝나면(모두 true를 반환하는 i가 없으면),
-        // 후입 선출 순서로 마지막 dfs의 제자리에 0을 반환하고 이전 idx로 돌아가서 for문을 마저 돈다.
-        // -- 반복 --
+        // 2. dfs가 끝나면(모두 true를 반환하는 i가 없으면),
+        //    후입 선출 순서로 마지막 dfs의 제자리에 0을 반환하고 이전 idx로 돌아가서 for문을 마저 돈다. -> 1
         if check_row(x, i, v) && check_col(y, i, v) && check_sqr(x, y, i, v) {
             v[x][y] = i;
             dfs(idx + 1, blank, v, output);
