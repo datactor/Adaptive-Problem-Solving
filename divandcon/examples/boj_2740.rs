24efbcc4324a0f3538bcefcf43a14501ff740d18
jaemin <datactress@gmail.com>
2023-01-25 16:05:30 +0900

    feat: matrix operation

---
+++ divandcon/examples/boj_2740.rs
@@ -0,0 +1,35 @@
+use std::{
+    io::{self, prelude::*, BufWriter},
+    error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut input = String::new();
+    let mut output = BufWriter::new(io::stdout().lock());
+    io::stdin().read_to_string(&mut input)?;
+
+    let mut lines = input.lines();
+    let mut n_m = lines.next().unwrap().split_ascii_whitespace().map(|s| s.parse::<usize>()).flatten();
+    let (n, m) = (n_m.next().unwrap(), n_m.next().unwrap());
+
+    let mut a: Vec<Vec<i32>> = (0..n).map(|_| lines.next().unwrap().split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
+    ).collect();
+
+    let mut k = lines.next().unwrap().split_ascii_whitespace().skip(1).next().unwrap().parse::<usize>().unwrap();
+
+    let mut b: Vec<Vec<i32>> = (0..m).map(|_| lines.next().unwrap().split_ascii_whitespace()
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect()
+    ).collect();
+
+    for r in a {
+        for i in 0..k {
+            let sum: i32 = r.iter().enumerate().map(|(j, num)| num * b[j][i]).sum();
+            write!(output, "{} ", sum)?;
+        } writeln!(output)?;
+    }
+
+    Ok(())
+}
---
+++ divandcon/target/debug/examples/boj_2740.d
@@ -0,0 +1,1 @@
+/home/jam/workspace/rust/training/ps/divandcon/target/debug/examples/boj_2740: /home/jam/workspace/rust/training/ps/divandcon/examples/boj_2740.rs
