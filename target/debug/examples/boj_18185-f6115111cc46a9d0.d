07fac0c22763a40de2e502f27c44f3933f661d47
jaemin <datactress@gmail.com>
2023-02-23 13:24:16 +0900

    feat: greedy

--- target/debug/examples/boj_18185-f6115111cc46a9d0.d
+++
@@ -1,5 +0,0 @@
-/home/jam/workspace/rust/training/ps/target/debug/examples/boj_18185-f6115111cc46a9d0: examples/greedy/boj_18185.rs
-
-/home/jam/workspace/rust/training/ps/target/debug/examples/boj_18185-f6115111cc46a9d0.d: examples/greedy/boj_18185.rs
-
-examples/greedy/boj_18185.rs:
