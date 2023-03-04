209a62e52d06adc6c7eeb604544ca7d5e3dd96d2
jaemin <datactress@gmail.com>
2023-03-04 19:56:36 +0900

    feat: fermat_little_theorem

--- src/lib.rs
+++ src/lib.rs
@@ -16,27 +16,50 @@ pub fn combinations<T>(data: &[T], r: usize) -> Vec<Vec<&T>> {
     result
 }
 
-// O(n + lgp)
-pub fn fermat_little_theorem(n: u128, r: u128, p: u128) -> u128{
-    fn fac(s: u128, n: u128) -> u128 {
-        let mut tmp = 1;
-        for i in s..n + 1 {
-            tmp = (tmp * i) % P;
+/// Fermat's little Theorem
+///
+/// p가 소수이면, 모든 정수 a에 대해 a^p ≡ a (mod p)이다.
+/// 혹은 p가 소수이고 a가 p의 배수가 아니면, a^(p-1) ≡ 1 (mod p)이다.
+/// 즉, a^(p-1)을 p로 나눈 나머지는 1이다.
+///
+///
+/// 페르마의 소정리에 의해 분모의 수를 아래와 같이 바꿀 수 있다.
+///
+/// B^p-1 ≡ 1 (mod p)
+///
+/// B*B^(p-2) ≡ 1 (mod p)
+///
+/// B^(p-2) ≡ B^(-1) (mod p)
+///
+/// AB^(-1) % p = AB^(p-2)%p = (A%p)*(B^(p-2)%p)%p
+///
+/// 위의 식을 이항계수 공식에 대입하면
+///
+///
+/// (n!/k!(n-k)!)%p = n!%p * (k!(n-k)!)^(p-2) % p
+///
+///                 = (n!/k!)%p * (n-k)!^(p-2) % p
+///
+/// O(n + lgp)
+pub fn fermat_little_theorem(n: u128, r: u128, p: u128) -> u128 {
+    fn fac(start: u128, end: u128) -> u128 {
+        let mut result = 1;
+        for i in start..=end {
+            result = (result * i) % p;
         }
-        tmp
+        result
     }
 
-    fn sqr(a: u128, b: u128) -> u128 {
-        match b {
+    fn mod_pow(base: u128, exponent: u128) -> u128 {
+        match exponent {
             0 => 1,
-            b if b % 2 == 1 => (sqr(a, b - 1) * a) % P,
-            _ => (sqr(a, b / 2).pow(2)) % P,
+            e if e % 2 == 1 => (mod_pow(base, e - 1) * base) % p,
+            _ => (mod_pow(base, exponent / 2).pow(2)) % p,
         }
     }
 
-    let a = fac(r + 1, n);
-    let b = fac(2, n - r) % p;
+    let numerator = fac(r + 1, n);
+    let denominator = fac(2, n - r) % p;
 
-    return (a % p) * sqr(b, p - 2) % p;
+    (numerator % p) * mod_pow(denominator, p - 2) % p
 }
-
