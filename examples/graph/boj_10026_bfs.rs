a8da9cd5adea323498734c55fec961775f393df4
jaemin <datactress@gmail.com>
2023-03-06 00:39:09 +0900

    feat: bfs

--- examples/graph/boj_10026_bfs.rs
+++ examples/graph/boj_10026_bfs.rs
@@ -1,101 +1,71 @@
-use std::{collections::VecDeque, io};
+use std::{
+    io::{self, prelude::*},
+    collections::VecDeque,
+};
 
-fn input() -> Vec<i32> {
-    let mut s = String::new();
-
-    io::stdin().read_line(&mut s).unwrap();
-
-    let values: Vec<i32> = s
-        .as_mut_str()
-        .split_whitespace()
-        .map(|s| s.parse().unwrap())
-        .collect();
-
-    values
-}
-
-fn as_byte(c: char) -> u8 {
-    match c {
-        'R' => 1,
-        'G' => 2,
-        'B' => 3,
-        _ => unreachable!(),
-    }
-}
-
-fn bfs(table: &Vec<Vec<u8>>, visited: &mut Vec<Vec<bool>>, n: usize, color: u8) -> i32 {
-    let mut q = VecDeque::new();
-    let mut cnt = 0;
-
-    for i in 0..n {
-        for j in 0..n {
-            if table[i][j] == color && !visited[i][j] {
-                q.push_back((j as i32, i as i32));
-                visited[i][j] = true;
-                cnt += 1;
-            }
-
-            while let Some((cur_x, cur_y)) = q.pop_front() {
-                for (dx, dy) in &[(-1, 0), (1, 0), (0, -1), (0, 1)] {
-                    let next_x = cur_x + dx;
-                    let next_y = cur_y + dy;
-
-                    if next_x < 0 || next_x >= n as i32 || next_y < 0 || next_y >= n as i32 {
-                        continue;
-                    }
-
-                    if visited[next_y as usize][next_x as usize] {
-                        continue;
-                    }
-
-                    if table[next_y as usize][next_x as usize] == color {
-                        q.push_back((next_x, next_y));
-                        visited[next_y as usize][next_x as usize] = true;
-                    }
-                }
-            }
-        }
-    }
-
-    cnt
-}
+const DX: [i32; 4] = [-1, 1, 0, 0];
+const DY: [i32; 4] = [0, 0, -1, 1];
 
 fn main() {
-    let n = input()[0] as usize;
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
 
-    let mut table = vec![vec![0; n]; n];
-    let mut visited = vec![vec![false; n]; n];
+    let mut lines = input.lines();
 
-    for i in 0..n {
-        let mut s = String::new();
-        io::stdin().read_line(&mut s).unwrap();
-        let mut chars = s.chars();
+    let mut read = || lines.next().unwrap();
+    let n = read().parse::<usize>().unwrap();
 
-        for j in 0..n {
-            table[i][j] = as_byte(chars.next().unwrap());
-        }
+    let mut graph = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        graph.push(read().as_bytes().to_owned());
     }
 
-    print!(
-        "{} ",
-        bfs(&table, &mut visited, n, 1)
-            + bfs(&table, &mut visited, n, 2)
-            + bfs(&table, &mut visited, n, 3)
-    );
+    let mut cont_graph = graph.clone();
 
     for i in 0..n {
         for j in 0..n {
-            visited[i][j] = false;
-
-            if table[i][j] == 2 {
-                table[i][j] = 1;
+            if graph[i][j] == b'G' {
+                cont_graph[i][j] = b'R'
             }
         }
     }
 
-    print!(
-        "{}",
-        bfs(&table, &mut visited, n, 1)
-            + bfs(&table, &mut visited, n, 3)
-    );
+    let mut cnt = 0;
+    let mut cnt2 = 0;
+    for i in 0..n {
+        for j in 0..n {
+            if graph[i][j] != 0 {
+                bfs(i, j, graph[i][j] as usize, &mut graph, n);
+                cnt += 1
+            }
+
+            if cont_graph[i][j] != 0 {
+                bfs(i, j, cont_graph[i][j] as usize, &mut cont_graph, n);
+                cnt2 += 1
+            }
+        }
+    }
+
+    println!("{} {}", cnt, cnt2);
+}
+
+fn bfs(x: usize, y: usize, color: usize, graph: &mut Vec<Vec<u8>>, n: usize) {
+    let mut q = VecDeque::from([(x, y)]);
+    graph[x][y] = 0;
+
+    while !q.is_empty() {
+        let (x, y) = q.pop_front().unwrap();
+
+        for i in 0..4 {
+            let (nx, ny) = (x as i32 + DX[i], y as i32 + DY[i]);
+            if nx < 0 || nx >= n as i32 || ny < 0 || ny >= n as i32 {
+                continue
+            }
+            if graph[nx as usize][ny as usize] as usize == color {
+                graph[nx as usize][ny as usize] = 0;
+                q.push_back((nx as usize, ny as usize));
+            }
+        }
+    }
 }
