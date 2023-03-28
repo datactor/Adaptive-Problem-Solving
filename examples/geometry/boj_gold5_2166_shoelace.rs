1fba5a6c54a1b3e4b6a5864ae5747013927daa06
jaemin <datactress@gmail.com>
2023-03-28 14:31:13 +0900

    feat: geometry: shoelace formula

--- examples/geometry/boj_gold5_2166_shoelace.rs
+++ examples/geometry/boj_gold5_2166_shoelace.rs
@@ -1,10 +1,13 @@
 // https://www.acmicpc.net/problem/2166
-// shoelace formula - https://ko.wikipedia.org/wiki/%EC%8B%A0%EB%B0%9C%EB%81%88_%EA%B3%B5%EC%8B%9D
+// shoelace formula
+// https://namu.wiki/w/%EC%8B%A0%EB%B0%9C%EB%81%88%20%EA%B3%B5%EC%8B%9D
+// https://ko.wikipedia.org/wiki/%EC%8B%A0%EB%B0%9C%EB%81%88_%EA%B3%B5%EC%8B%9D
 
-use std::io::{self, prelude::*};
+use std::io::{self, prelude::*, BufWriter};
 
 fn main() -> io::Result<()> {
     let mut input = io::stdin().lock().lines();
+    let mut output = BufWriter::new(io::stdout().lock());
 
     let mut line = || input.next().unwrap().unwrap();
     let n = line().parse::<usize>().unwrap();
@@ -31,11 +34,12 @@ fn main() -> io::Result<()> {
         sum += xs.0 * ys.1 - xs.1 * ys.0;
     }
 
+    // 첫번째 점과 마지막 점이 동일하지 않을 경우를 대비
     if x0 != xs.1 || y0 != ys.1 {
         sum += xs.1 * y0 - x0 * ys.1;
     }
 
-    println!("{:.1}", (sum as f64 / 2.0).abs());
+    writeln!(output, "{:.1}", (sum as f64 / 2.0).abs())?;
 
     Ok(())
 }
