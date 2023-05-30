07484dd9eadb1b6a33cea02b501cf4b7aa701df7
jaemin <datactress@gmail.com>
2023-05-30 17:58:49 +0900

    feat: bfs

--- examples/graph/boj_s2_2644_bfs.rs
+++ examples/graph/boj_s2_2644_bfs.rs
@@ -4,19 +4,19 @@ use std::{
     io::{self, Write, BufRead, BufReader, BufWriter},
 };
 
-macro_rules! read_to_num {
+macro_rules! read_to_nums {
     ($reader:expr, $input:expr, $type:ty) => {
         {
             $reader.read_line(&mut $input)?;
             let n = $input.trim().parse::<$type>();
             $input.clear();
             $reader.read_line(&mut $input)?;
-            let (a, b) = $input.trim().split_once(' ').expect("no a & b");
-            let (a, b) = (a.parse::<$type>(), b.parse::<$type>());
+            let (x, y) = $input.trim().split_once(' ').expect("no x & y");
+            let (x, y) = (x.parse::<$type>(), y.parse::<$type>());
             $input.clear();
             $reader.read_line(&mut $input)?;
             let m = $input.trim().parse::<$type>();
-            (n, a, b, m)
+            (n, x, y, m)
         }
     }
 }
@@ -38,18 +38,21 @@ macro_rules! read_to_vec {
     }
 }
 
-fn bfs(graph: &mut Vec<Vec<i8>>, n: usize, x: i8, y: usize) -> i8 {
+fn bfs(graph: &mut Vec<Vec<i8>>, n: usize, x: i8, y: usize) -> io::Result<i8> {
     let mut degree_with_x = vec![0; n+1];
-    let mut dq = Vec::from([x]);
-    while let Some(a) = dq.pop() {
-        for i in &graph[a as usize] {
+    let mut q = std::collections::VecDeque::from([x]);
+    while let Some(relation) = q.pop_front() {
+        for i in &graph[relation as usize] {
             if degree_with_x[*i as usize] == 0 {
-                degree_with_x[*i as usize] = degree_with_x[a as usize] + 1;
-                dq.push(*i);
+                degree_with_x[*i as usize] = degree_with_x[relation as usize] + 1;
+                q.push_back(*i);
             }
         }
     }
-    degree_with_x[y]
+    degree_with_x.get(y)
+        .cloned()
+        .map(|n| if n == 0 { -1 } else { n })
+        .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "The y is out of 0..=n"))
 }
 
 fn main() -> io::Result<()> {
@@ -57,10 +60,9 @@ fn main() -> io::Result<()> {
     let mut write_buf = BufWriter::new(io::stdout().lock());
     let mut buf_to_string = String::new();
 
-    if let (Ok(n), Ok(x), Ok(y), Ok(m)) = read_to_num!(read_buf, buf_to_string, usize) {
+    if let (Ok(n), Ok(x), Ok(y), Ok(m)) = read_to_nums!(read_buf, buf_to_string, usize) {
         let mut relations = read_to_vec!(read_buf, buf_to_string, n, m, i8);
-        let degree_between_x_y = bfs(&mut relations, n, x as i8, y);
-        writeln!(write_buf, "{:?}", if degree_between_x_y > 0 { degree_between_x_y } else { -1 })?
+        writeln!(write_buf, "{}", bfs(&mut relations, n, x as i8, y)?)?
     }
 
     Ok(())
