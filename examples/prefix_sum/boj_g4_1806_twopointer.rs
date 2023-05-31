86abb89e4d70e5744adc7aee9197d78d9ab6f4fe
jaemin <datactress@gmail.com>
2023-05-31 14:14:51 +0900

    feat: two pointer

--- examples/prefix_sum/boj_g4_1806_twopointer.rs
+++ examples/prefix_sum/boj_g4_1806_twopointer.rs
@@ -5,38 +5,33 @@ use std::{
     io::{self, Write, BufRead, BufReader, BufWriter},
 };
 
-macro_rules! read_to_nums {
-    ($reader:expr, $input:expr, $type:ty) => {
+macro_rules! read_nums {
+    ($reader:expr, $type:ty) => {
         {
-            $reader.read_line(&mut $input)?;
-            let ns = $input.trim().split_once(' ').expect("no n & s");
-            let (n, s) = (ns.0, ns.1);
-            (n.parse::<$type>(), s.parse::<i32>())
+            let line = $reader.next().expect("Failed to get next line").expect("Failed to read the line");
+            line
+                .split_ascii_whitespace() // ascii 문자열 범위 내에서만 처리하기 때문에 범위가 한정되어있어 일반적으로 더 빠름
+                .map(|s| s.parse::<$type>().expect(&format!("Failed to parse '{}' into a '{}'", s, stringify!($type))))
+                .collect::<Vec<$type>>()
         }
     }
 }
 
 macro_rules! find_len {
-    ($reader:expr, $input:expr, $n:expr, $s:expr, $type:ty) => {
+    ($table:expr, $n:expr, $s:expr, $type:ty) => {
         {
-            $input.clear();
-            $reader.read_line(&mut $input)?;
-            let table = $input
-                            .split_ascii_whitespace()
-                            .map(|s| s.parse::<$type>().expect("Failed to parse"))
-                            .collect::<Vec<$type>>();
             let mut min_len = i32::MAX;
             let (mut left, mut right, mut sum) = (0, 0, 0);
 
             loop {
-                if sum >= $s {
+                if sum >= $s as i32 {
                     min_len = min_len.min(right - left);
-                    sum -= table[left as usize];
+                    sum -= $table[left as usize];
                     left += 1;
                 } else if right == $n as i32 {
                     break;
                 } else {
-                    sum += table[right as usize];
+                    sum += $table[right as usize];
                     right += 1;
                 }
             }
@@ -51,12 +46,12 @@ macro_rules! find_len {
 }
 
 fn main() -> io::Result<()> {
-    let mut read_buf = BufReader::new(io::stdin().lock());
+    let mut read_buf = BufReader::new(io::stdin().lock()).lines();
     let mut write_buf = BufWriter::new(io::stdout().lock());
-    let mut buf_to_string = String::new();
 
-    if let (Ok(n), Ok(s)) = read_to_nums!(read_buf, buf_to_string, usize) {
-        writeln!(write_buf, "{}", find_len!(read_buf, buf_to_string, n, s, i32))?;
-    }
+    let params = read_nums!(read_buf, usize);
+    let vec = read_nums!(read_buf, i32);
+    writeln!(write_buf, "{}", find_len!(vec, params[0], params[1], i32))?;
+
     Ok(())
 }
