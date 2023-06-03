fcd355384d71071033e510977e755093d65800a7
jaemin <datactress@gmail.com>
2023-06-03 23:38:27 +0900

    feat: hashset

--- examples/bisec/boj_1920.rs
+++ examples/bisec/boj_1920.rs
@@ -1,44 +1,44 @@
 // https://www.acmicpc.net/problem/1920
-// 이분탐색이 아니라 control group을 hashset으로 중복값 없이 만들어 단순 for문 서치함
+// 이분탐색이 아니라 control group을 hashset으로 O(1)로 색인함
 
 use std::{
     collections::HashSet,
     error::Error,
-    io::{self, prelude::*, BufWriter},
+    io::{self, Write, BufRead, BufReader, BufWriter},
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut input = String::new();
+    let mut lines = BufReader::new(io::stdin().lock()).lines();
     let mut output = BufWriter::new(io::stdout().lock());
-    io::stdin().read_to_string(&mut input)?;
+    lines.next();
 
-    let mut input = input.lines();
-    input.next();
-
-    let n_list: HashSet<i32> = input
+    let n_list: HashSet<i32> = lines
         .next()
+        .unwrap()
         .unwrap()
         .split_ascii_whitespace()
         .map(|s| s.parse::<i32>().unwrap())
         .collect();
 
-    input
+    lines
         .skip(1)
         .next()
         .unwrap()
+        .unwrap()
         .split_ascii_whitespace()
-        .map(|s| s.parse::<i32>().unwrap())
-        .for_each(|i| {
-            writeln!(
-                output,
-                "{}",
-                match n_list.contains(&i) {
-                    true => 1,
-                    false => 0,
-                }
-            )
-            .unwrap()
-        });
+        .for_each(
+            |s| {
+                let i = s.parse::<i32>().unwrap();
+                writeln!(
+                    output,
+                    "{}",
+                    match n_list.contains(&i) {
+                        true => 1,
+                        false => 0,
+                    }
+                ).unwrap()
+            }
+        );
 
     Ok(())
 }
