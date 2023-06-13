66563eb3b7f48ca4e1ca58eecf1d9ec95b5ea48d
jaemin <datactress@gmail.com>
2023-06-13 19:07:00 +0900

    feat: str

--- boj/str/boj_s4_11656_sorting.rs
+++ boj/str/boj_s4_11656_sorting.rs
@@ -2,35 +2,33 @@
 
 use std::{
     io::{self, Write, BufRead, BufWriter},
-    cmp::Ordering,
 };
 
-fn main() -> io::Result<()> {
-    let mut write_buf = BufWriter::new(io::stdout().lock());
-    let mut input = String::new();
+macro_rules! ok {
+    (()) => {
+        {
+            let mut input = String::new();
+            io::stdin().lock().read_line(&mut input)?;
 
-    io::stdin().lock().read_line(&mut input)?;
+            let mut write_buf = BufWriter::new(io::stdout().lock());
 
-    let mut v: Vec<String> = Vec::new();
+            let mut v = Vec::new();
+            for i in 0..input.trim().len() {
+                let tmp = input[i..].to_string();
+                v.push(tmp);
+            }
 
-    for i in 0..input.trim().len() {
-        let tmp = input[i..].to_string();
-        v.push(tmp);
-    }
+            v.sort_by(|a, b| a.cmp(b));
 
-    v.sort_by(|a, b| {
-        if a < b {
-            Ordering::Less
-        } else if a > b {
-            Ordering::Greater
-        } else {
-            Ordering::Equal
+            for element in v {
+                write!(write_buf, "{}", element)?;
+            }
+
+            Ok(())
         }
-    });
-
-    for element in v {
-        write!(write_buf, "{}", element)?;
     }
+}
 
-    Ok(())
+fn main() -> io::Result<()> {
+    ok!(())
 }
