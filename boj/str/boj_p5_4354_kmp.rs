8d146dcbed78e330231ba2e33619f13d17ab07e8
jaemin <datactress@gmail.com>
2023-06-14 17:15:33 +0900

    feat: kmp

--- boj/str/boj_p5_4354_kmp.rs
+++ boj/str/boj_p5_4354_kmp.rs
@@ -21,24 +21,23 @@ macro_rules! ok {
             let mut write_buf = BufWriter::new(io::stdout().lock());
             let mut buf_to_string = String::new();
 
-            while let billboard = read!(read_buf, buf_to_string) {
-                if billboard[0] == b'.' { break }
-                let len = billboard.len();
+            while let board = read!(read_buf, buf_to_string) {
+                if board.first().expect("Empty space") == &b'.' { break }
+                let len = board.len();
                 let mut table: Vec<usize> = vec![0; len];
                 let mut pi = 0;
-                for (i, b) in billboard.iter().enumerate().skip(1) {
-                    while pi > 0 && &billboard[pi] != b {
-                        pi = table[pi - 1];
+                for (i, b) in board.iter().enumerate().skip(1) {
+                    while pi > 0 && &board[pi] != b {
+                        pi = table[pi-1];
                     }
-                    if &billboard[pi] == b {
+                    if &board[pi] == b {
                         pi += 1;
                         table[i] = pi;
                     }
                 }
 
                 let val = table.last().cloned().unwrap_or(0);
-
-                writeln!(write_buf, "{}", if len % (len - val) != 0 { 1 } else { len / (len - val) })?;
+                writeln!(write_buf, "{}", if len % (len-val) != 0 { 1 } else { len / (len-val) })?;
             }
 
             Ok(())
