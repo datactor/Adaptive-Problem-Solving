6fc18b9696998a42e9dc9ac0a28cafde0345dc1b
jaemin <datactress@gmail.com>
2023-06-27 21:59:01 +0900

    feat: math & geometry

--- boj/geometry/boj_1002.rs
+++ boj/geometry/boj_1002.rs
@@ -1,32 +1,38 @@
-use std::io::{self, prelude::*, BufWriter};
+// https://www.acmicpc.net/problem/1002
 
-fn main() {
+use std::{
+    io::{self, Read, Write, BufWriter},
+    error::Error,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
     let mut output = BufWriter::new(io::stdout().lock());
     let mut buffer = String::new();
-    io::stdin().read_to_string(&mut buffer).unwrap();
+    io::stdin().read_to_string(&mut buffer)?;
 
     let mut lines = buffer.lines();
-    let n = lines.next().unwrap().parse::<usize>().unwrap();
+    let n = lines.next().ok_or("No lines")?.parse::<usize>()?;
 
-    for i in 0..n {
+    for _ in 0..n {
         let mut v = lines
             .next()
-            .unwrap()
+            .ok_or("Reached out of lines")?
             .split_ascii_whitespace()
-            .map(|s| s.parse::<i32>())
-            .flatten();
+            .map(|s| s.parse::<i32>().unwrap());
         let (x1, y1, r1) = (v.next().unwrap(), v.next().unwrap(), v.next().unwrap());
         let (x2, y2, r2) = (v.next().unwrap(), v.next().unwrap(), v.next().unwrap());
 
         let dst = (((x1 - x2).pow(2) + (y1 - y2).pow(2)) as f32).powf(0.5);
-        if dst == 0.0 && r1 == r2 {
-            writeln!(output, "-1").unwrap();
+        let ans = if dst == 0.0 && r1 == r2 {
+            -1
         } else if dst == (r1 + r2) as f32 || (r1 - r2).abs() as f32 == dst {
-            writeln!(output, "1").unwrap();
+            1
         } else if dst < (r1 + r2) as f32 && dst > (r1 - r2).abs() as f32 {
-            writeln!(output, "2").unwrap();
+            2
         } else {
-            writeln!(output, "0").unwrap();
-        }
+            0
+        };
+        writeln!(output, "{}", ans)?;
     }
+    Ok(())
 }
